e605bbc9618291fb5a5f50ea235c45d8c4c126e5
ZeinaAbdelshafy <zeina.waeil.zw@gmail.com>
2026-04-24 13:04:23 +0300

    version 1 project separation of responsibility

---
+++ config.py
@@ -0,0 +1,18 @@
+# Sampling & Channels
+FS = 250
+DURATION = 300.0
+N_CHANNELS = 8
+
+# Signal Processing
+BANDPASS_LOW = 0.5
+BANDPASS_HIGH = 45.0
+FILTER_ORDER = 4
+ASR_CALIB_S = 3.0
+ASR_WIN_S = 0.5
+ASR_CUTOFF = 2.5
+
+# Detection & Baseline
+BASELINE_DURATION = 10.0
+THRESHOLD_Z = 2.0
+WINDOW_S = 2.0   # Welch window length
+STEP_S = 0.5     # Update interval
